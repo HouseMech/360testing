d791271adc357315d48052283b49a566d30eebb0
Liam Harbec <liam.harbec@gmail.com>
2021-04-15 11:21:37 -0700

    Testing more pathing

--- layouts/global_head_include.php
+++ layouts/global_head_include.php
@@ -2,8 +2,8 @@
   <meta charset="utf-8">
   <title>MyBlogPost</title>
   <?php $path = $_SERVER['DOCUMENT_ROOT'];?>
-  <link rel="stylesheet" href="<?php echo $path; ?>/css/style.css">
-  <link rel="stylesheet" href="<?php echo $path; ?>/css/header.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/header.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
