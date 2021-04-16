3af39586925061945ca3a100a0b7e2e54b2e6394
Liam Harbec <liam.harbec@gmail.com>
2021-04-16 13:40:25 -0700

    test

--- php/search.php
+++ php/search.php
@@ -119,7 +119,7 @@
 
             // Display post title.
             echo "<div id='blogPost'>";
-            echo "<h2>" . $postTitle . "<img class='arrow' src='/img/pageImgs/up_arrow.png' height=25 width=25></h2>";
+            echo "<h2>" . $postTitle . "<img class='arrow' src='./img/pageImgs/up_arrow.png' height=25 width=25></h2>";
 
 
             // div needed for collapsible post
